1c63b6cf0023a348978f7af57b6e193f6a74bd69
alrick19 <alrick.vincent@gmail.com>
2023-04-07 18:29:47 -0700

    switched headers to be reactive for mobile devices and small pages

--- routes/headers/header1.php
+++ routes/headers/header1.php
@@ -1,36 +1,36 @@
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content = "width=device-width, intial-scale=1.0">
     <link rel="stylesheet" href="../css/header.css">
 </head>
 <header>
     <?php session_start();?>
-    <div class="header">
-        <a href="home.php" class="logo"><img src="../layout_and_logic_docs/Project_logo_roughdraft.png" width="150", height="80"></a>
-        <nav>
-            <form action="../searchpost.php" method="POST">
-                <input id="search" type="search" placeholder="Search Ski-it">
-            </form>
-            <ul>
-                <li><a class="home" href="home.php">Home</a></li>
-                <?php
-                include 'shortcuts.php';
+    <div class="header-container">
+            <a href="home.php" class="logo"><img src="../layout_and_logic_docs/Project_logo_roughdraft.png" width="150" height="80" alt="Logo"></a>
+            <nav>
+                <form action="searchpost.php" method="POST">
+                    <input id="search" type="search" placeholder="Search Ski-it" name="search">
+                </form>
+                <ul>
+                    <li><a class="home" href="home.php">Home</a></li>
+                    <?php
+                    include 'shortcuts.php';
 
-                if(isset($_SESSION['username'])){
-                    if(isAdmin($_SESSION['username'])){
-                        echo "<li><a class=\"login\" href=\"admin.php\">Admin</a></li>";
-                    }else{
-                        echo "<li><a class=\"login\" href=\"account.php\">My Account</a></li>";
+                    if (isset($_SESSION['username'])) {
+                        if (isAdmin($_SESSION['username'])) {
+                            echo "<li><a class=\"login\" href=\"admin.php\">Admin</a></li>";
+                        } else {
+                            echo "<li><a class=\"login\" href=\"account.php\">My Account</a></li>";
+                        }
+                        echo "<li><a class=\"login\" href=\"logout.php\">Sign Out</a></li>";
+                    } else {
+                        echo "<li><a class=\"login\" href=\"login.php\">Log In</a></li>";
                     }
-                    echo "<li><a class=\"login\" href=\"logout.php\">Sign Out</a></li>";
-                    if(isAdmin($_SESSION['username'])){
-                    }
-                }else{
-                    echo "<li><a class=\"login\" href=\"login.php\">Log In</a></li>";
-                }
-                ?>
-            </ul>
-        </nav>
-    </div>
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </header>
     
 </header>
 <div id="backgroundlogo"></div>
