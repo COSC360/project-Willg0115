ad713cf19873eb989655959111e1151b96590c9e
Willg0115 <willg0115@outlook.com>
2023-04-07 21:54:25 -0700

    changed connection details to server

--- routes/dbConnection.php
+++ routes/dbConnection.php
@@ -1,14 +1,14 @@
 <?php
     
-    // $host = "cosc360.ok.ubc.ca";
-    // $database = "db_63271324";
-    // $user = "63271324";
-    // $pass = "63271324";
+    $host = "cosc360.ok.ubc.ca";
+    $database = "db_63271324";
+    $user = "63271324";
+    $pass = "63271324";
 
-    $host = "localhost";
-    $database = "skiit";
-    $user = "webuser";
-    $pass= "P@ssw0rd";
+    // $host = "localhost";
+    // $database = "skiit";
+    // $user = "webuser";
+    // $pass= "P@ssw0rd";
     
     try{
         $pdo = new PDO("mysql:host=$host;dbname=$database",$user, $pass);
